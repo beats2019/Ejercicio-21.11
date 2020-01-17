5a39629033a0df0c432016de9ed45bfb5679c15b
Jose <joselavila13@hotmail.com>
2020-01-17 17:25:01 -0500

    Printing the bits of unsigned variables

--- Ejercicio 21.11.cpp
+++ Ejercicio 21.11.cpp
@@ -2,10 +2,12 @@
 #include <string>
 using namespace std;
 string charToBits(unsigned char);
+void printBits(unsigned);
+
 
 int main()
 {
-    cout << charToBits('K');
+    printBits('a');
 }
 
 
@@ -13,8 +15,19 @@ string charToBits(unsigned char value)
 {
     string resultString("");
     for (int i = 0; i < sizeof(value)*8; i++)
-    {
         resultString.insert(0,to_string((value >> i) & 1));
-    }
+    
     return resultString;
 }
+
+void printBits(unsigned value)
+{
+    const int limit = 8 * sizeof(unsigned) - 1;
+    unsigned mask = 1 << limit;
+    for (unsigned i = 1; i <= limit+1; i++)
+    {
+        cout << ((value << i) & mask ? "1" : "0");
+        if (i % 8 == 0)
+            cout << ' ';
+    }
+ }
